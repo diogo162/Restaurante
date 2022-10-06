eb0d284caed3ecc9ddea7d0b20131e34f0b1c93a
diogo162 <albanezdiogo@gmail.com>
2022-10-06 00:28:56 -0300

    to ficando louco

--- src/main/java/org/example/Endereco.java
+++ src/main/java/org/example/Endereco.java
@@ -1,8 +1,16 @@
 package org.example;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Endereco {
+
+
     private String bairro;
     private String rua;
     private String numero;
     private String APnum;
+
+
 }
