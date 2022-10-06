efd323fbe6cb46bb1bd4a47a91c320733f0c5a3d
diogo162 <albanezdiogo@gmail.com>
2022-10-05 22:37:36 -0300

    criação do banco e começo de interatividade entre programa e BD

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,5 +2,17 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-b
-2src/main/java/org/example/MovimentacaoEstoque.java,a\9\a90a5224cedf479346659454b685a3bc04e26032
+W
+'src/main/java/org/example/Endereco.java,d\5\d56ea5608ec98004756415538179d20de1b7b3e5
+V
+&src/main/java/org/example/Gerente.java,1\0\10f18cca7cacc4ee589d559736879f40b5fc91c9
+Z
+*src/main/java/org/example/Funcionario.java,4\a\4a53f95fa4617565285be9cbff03cdb1f5508df1
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/org/example/Cardapio.java,b\c\bc437fd9fdf86d12bb0729f634bfb34236fde774
+V
+&src/main/java/org/example/Cliente.java,1\3\13bcc8b8ece17bfd40033ef485dd95f5f52e445b
+Y
+)src/main/java/org/example/Entregador.java,7\0\7083e692a54c801d03be51b4819b4ddbb8323a46
--- src/main/java/org/example/Pessoa.java
+++ src/main/java/org/example/Pessoa.java
@@ -1,16 +1,46 @@
 package org.example;
 
 import com.sun.istack.NotNull;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.UniqueConstraint;
+@Entity
 public class Pessoa {
     @NotNull
     private String nome;
     @Id
     @NotNull
-    private String CPF;
+    private String cpf;
     private int telefone_celular;
     @NotNull
-    private Endereco endereco;
+    private String endereco;
+
+
+    public Pessoa() {
+        //O Hibernate necessita de um construtor sem parametros
+    }
+
+
+    public Pessoa(String cpf, String nome) {
+        super();
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
